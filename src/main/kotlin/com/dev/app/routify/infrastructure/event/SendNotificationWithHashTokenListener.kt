b11f9e791d035031b84c957d0fbc843c09ede6df
TorHugo <arruda.victorhugo@outlook.com>
2025-02-17 09:58:38 -0300

    FIXED(coroutines): - Add delay to executable onSendNotificationWithHashTokenListener.

--- src/main/kotlin/com/dev/app/routify/infrastructure/event/SendNotificationWithHashTokenListener.kt
+++ src/main/kotlin/com/dev/app/routify/infrastructure/event/SendNotificationWithHashTokenListener.kt
@@ -15,6 +15,7 @@ import com.dev.app.routify.domain.service.NotificationEmailService
 import com.dev.app.routify.infrastructure.event.models.SendNotificationWithHashTokenEventDTO
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -30,51 +31,57 @@ class SendNotificationWithHashTokenListener(
     companion object {
         private const val DEFAULT_KEY_HASH_CODE: String = "hashcode"
         private const val DEFAULT_KEY_EXPIRATION_DATE: String = "expiration-date"
+        private const val DEFAULT_THEAD_DELAY: Long = 500L
     }
 
     @EventListener
     fun onSendNotificationWithHashTokenListener(dto: SendNotificationWithHashTokenEventDTO) {
         CoroutineScope(Dispatchers.IO).launch {
-            try {
-                logger.info("c=SendNotificationWithHashTokenListener m=onSendNotificationWithHashTokenListener() s=start email=${dto.user.email} type=${dto.typeToken}")
-                val token = createTokenUseCase.execute(dto.user, dto.typeToken.value)
-                val defaultTokenParameters = listOf(
-                    Parameter(
-                        key = DEFAULT_KEY_HASH_CODE,
-                        value = token.tokenHash
-                    ),
-                    Parameter(
-                        key = DEFAULT_KEY_EXPIRATION_DATE,
-                        value = token.expiration.toFormatted()
-                    )
-                )
-                val parameters = dto.parameters.plus(defaultTokenParameters)
-                val userDomain = dto.user.toDomain()
+            delay(DEFAULT_THEAD_DELAY)
+            handleSendNotificationWithHashTokenListener(dto = dto)
+        }
+    }
 
-                val mailTemplate = dto.template
-                val subject = mailTemplate.subject
-                val body = mailTemplate.message.replaceAllParameters(parameters)
-
-                val notificationDomain = NotificationDomain(
-                    userId = dto.user.identifier!!,
-                    toEmail = userDomain.email.value,
-                    status = StatusNotification(dto.statusNotification.value),
-                    type = TypeNotification(dto.typeNotification.value),
-                    subject = subject,
-                    message = body
+    suspend fun handleSendNotificationWithHashTokenListener(dto: SendNotificationWithHashTokenEventDTO) {
+        try {
+            logger.info("c=SendNotificationWithHashTokenListener m=onSendNotificationWithHashTokenListener() s=start email=${dto.user.email} type=${dto.typeToken}")
+            val token = createTokenUseCase.execute(dto.user, dto.typeToken.value)
+            val defaultTokenParameters = listOf(
+                Parameter(
+                    key = DEFAULT_KEY_HASH_CODE,
+                    value = token.tokenHash
+                ),
+                Parameter(
+                    key = DEFAULT_KEY_EXPIRATION_DATE,
+                    value = token.expiration.toFormatted()
                 )
+            )
+            val parameters = dto.parameters.plus(defaultTokenParameters)
+            val userDomain = dto.user.toDomain()
 
-                notificationEmailService.execute(
-                    domain = notificationDomain
-                )
-                logger.info("c=SendNotificationWithHashTokenListener m=onSendNotificationWithHashTokenListener() s=done email=${dto.user.email} type=${dto.typeToken}")
-            } catch (ex: DomainException) {
-                logger.info("c=SendNotificationWithHashTokenListener m=onSendNotificationWithHashTokenListener() s=error-domain email=${dto.user.email} type=${dto.typeToken} message=${ex.message}")
-                throw DomainException(ex.message)
-            } catch (ex: Exception) {
-                logger.info("c=SendNotificationWithHashTokenListener m=onSendNotificationWithHashTokenListener() s=error-generic email=${dto.user.email} type=${dto.typeToken} message=${ex.message}")
-                throw GenericException(ex.message ?: ErrorMessageEnum.INTERNAL_SERVER_ERROR.message)
-            }
+            val mailTemplate = dto.template
+            val subject = mailTemplate.subject
+            val body = mailTemplate.message.replaceAllParameters(parameters)
+
+            val notificationDomain = NotificationDomain(
+                userId = dto.user.identifier!!,
+                toEmail = userDomain.email.value,
+                status = StatusNotification(dto.statusNotification.value),
+                type = TypeNotification(dto.typeNotification.value),
+                subject = subject,
+                message = body
+            )
+
+            notificationEmailService.execute(
+                domain = notificationDomain
+            )
+            logger.info("c=SendNotificationWithHashTokenListener m=onSendNotificationWithHashTokenListener() s=done email=${dto.user.email} type=${dto.typeToken}")
+        } catch (ex: DomainException) {
+            logger.info("c=SendNotificationWithHashTokenListener m=onSendNotificationWithHashTokenListener() s=error-domain email=${dto.user.email} type=${dto.typeToken} message=${ex.message}")
+            throw DomainException(ex.message)
+        } catch (ex: Exception) {
+            logger.info("c=SendNotificationWithHashTokenListener m=onSendNotificationWithHashTokenListener() s=error-generic email=${dto.user.email} type=${dto.typeToken} message=${ex.message}")
+            throw GenericException(ex.message ?: ErrorMessageEnum.INTERNAL_SERVER_ERROR.message)
         }
     }
 }
